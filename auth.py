9b900fe6c3aa4c399be972f5d8d3e9e25cb5096e
Xav <xavier.stoa@gmail.com>
2024-12-22 20:39:23 +0100

    Fix

--- auth.py
+++ auth.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 # éxecuter "openssl rand -hex 32" pour obtenir une nouvelle clef
 SECRET_KEY = "73980ab468f0a5be1b530fbe5c7e43654aee29faf486bcd63040de51105b951a"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 5
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 class Token(BaseModel):
     access_token: str
