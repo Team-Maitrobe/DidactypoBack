fdafec34debaaa12a323a79d0ecae5fc978a7786
Jeremy <jereemy.garcia@gmail.com>
2024-12-28 16:42:00 +0100

    changement exo fichier sql

--- exercices.sql
+++ exercices.sql
@@ -1,2 +1,2 @@
-INSERT INTO EXERCICE (description_exercice, exercice_fait) VALUES
-('j f jjj fff jjff jffj qsdf jklm qm sl dk fj sd kl fq mj qjskdlfm', FALSE).
+INSERT INTO EXERCICE (titre_exercice, description_exercice) VALUES
+('Exercice 1 ', 'j f jjj fff jjff jffj qsdf jklm qm sl dk fj sd kl fq mj qjskdlfm').
